05c1eb6a8f630af631a530c477a43035bf2adc0c
Nathan Lam <nathancy@hawaii.edu>
2017-03-14 17:52:48 -1000

    Fixes

--- slideout.js
+++ slideout.js
@@ -1,13 +1,10 @@
-/**
- * Created by Nathan Lam on 3/9/2017.
- */
 var slideout = new Slideout({
   'panel': document.getElementById('main'),
-  'menu': document.getElementById('menu'),
+  'menu': document.getElementById('weatherbox-slideout'),
   'padding': 256,
   'tolerance': 70
 });
 
-document.querySelector('.js-slideout-toggle').addEventListener('click', function() {
+document.querySelector('.js-slideout-toggle').addEventListener('click', function () {
   slideout.toggle();
 });
